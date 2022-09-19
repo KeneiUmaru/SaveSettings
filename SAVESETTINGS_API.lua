11cf0d465d033e56aadb828a267943591a15fdb4
KeneiUmaru <113576821+KeneiUmaru@users.noreply.github.com>
2022-09-19 13:54:09 +0700

    Save

--- SAVESETTINGS_API.lua
+++ SAVESETTINGS_API.lua
@@ -1,30 +1,30 @@
-local getgenv()['MyName'] = game.Players.LocalPlayer.DisplayName
+local getgenv().MyName = game.Players.LocalPlayer.DisplayName
 
-local getgenv()['JsonEncode'] = function(msg)
+local getgenv().JsonEncode = function(msg)
     return game:GetService("HttpService"):JSONEncode(msg)
 end
-local getgenv()['JsonDecode'] = function(msg)
+local getgenv().JsonDecode = function(msg)
     return game:GetService("HttpService"):JSONDecode(msg)
 end
-local getgenv()['Check_Setting'] = function(Name)
+local getgenv().Check_Setting = function(Name)
     if not isfolder('DaiNo') then
         makefolder('DaiNo')
     end
     if not isfile('DaiNo/'..Name..'_'..'Config.json') then
-        writefile('DaiNo/'..Name..'_'..'Config.json',JsonEncode(getgenv().Setting))
+        writefile('DaiNo/'..Name..'_'..'Config.json',JsonEncode(getgenv().Config))
     end
 end
-local getgenv()['Get_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('DaiNo/'..Name..'_'..'Config.json') then
+local getgenv().Get_Setting = function(Name)
+    if isfolder('Script') and isfile('DaiNo/'..Name..'_'..'Config.json') then
         getgenv().Setting = JsonDecode(readfile('DaiNo/'..Name..'_'..'Config.json'))
-        return getgenv().Setting
+        return getgenv().Config
     else
         Check_Setting(Name)
     end
 end
-local getgenv()['Update_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('DaiNo/'..Name..'_'..'Config.json') then
-        writefile('DaiNo/'..Name..'_'..'Config.json',JsonEncode(getgenv().Setting))
+local getgenv().Update_Setting = function(Name)
+    if isfolder('Script') and isfile('DaiNo/'..Name..'_'..'Config.json') then
+        writefile('DaiNo/'..Name..'_'..'Config.json',JsonEncode(getgenv().Config))
     else
         Check_Setting(Name)
     end
