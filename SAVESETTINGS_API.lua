e98cb09834de8772d7b84a1fa028283f931e6b62
KeneiUmaru <113576821+KeneiUmaru@users.noreply.github.com>
2022-09-19 13:42:34 +0700

    Create SAVESETTINGS_API.lua
    It's SaveSettings

---
+++ SAVESETTINGS_API.lua
@@ -0,0 +1,31 @@
+local getgenv()['MyName'] = game.Players.LocalPlayer.DisplayName
+
+local getgenv()['JsonEncode'] = function(msg)
+    return game:GetService("HttpService"):JSONEncode(msg)
+end
+local getgenv()['JsonDecode'] = function(msg)
+    return game:GetService("HttpService"):JSONDecode(msg)
+end
+local getgenv()['Check_Setting'] = function(Name)
+    if not isfolder('DaiNo') then
+        makefolder('DaiNo')
+    end
+    if not isfile('DaiNo/'..Name..'_'..'Config.json') then
+        writefile('DaiNo/'..Name..'_'..'Config.json',JsonEncode(getgenv().Setting))
+    end
+end
+local getgenv()['Get_Setting'] = function(Name)
+    if isfolder('SaveSetting') and isfile('DaiNo/'..Name..'_'..'Config.json') then
+        getgenv().Setting = JsonDecode(readfile('DaiNo/'..Name..'_'..'Config.json'))
+        return getgenv().Setting
+    else
+        Check_Setting(Name)
+    end
+end
+local getgenv()['Update_Setting'] = function(Name)
+    if isfolder('SaveSetting') and isfile('DaiNo/'..Name..'_'..'Config.json') then
+        writefile('DaiNo/'..Name..'_'..'Config.json',JsonEncode(getgenv().Setting))
+    else
+        Check_Setting(Name)
+    end
+end
